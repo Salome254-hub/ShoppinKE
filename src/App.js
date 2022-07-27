e2e6e944f58d5a73e619b0b1b74e89e33a42195f
Salome <sallynanjala254@gmail.com>
2022-07-27 20:11:01 +0300

    Added a browserrouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './component/Header';
 function App() {
   return (
-    <div className="App" >
-      <Header/>
+    <BrowserRouter>
+    <Header/>
+    </BrowserRouter>
+      
 
-    </div>
+    
   );
 }
 
