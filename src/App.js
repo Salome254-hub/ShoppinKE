dc0541969042225b78f329c813d3ce99e352b8e5
Salome <sallynanjala254@gmail.com>
2022-07-27 21:25:38 +0300

    Added a Cart component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Header from './component/Header';
 function App() {
   return (
     <BrowserRouter>
     <Header/>
+    <div>
+      <Route path='/' exact>
+        <Home/>
+      </Route>
+      <Route path='/cart' exact>
+        <Cart/>
+      </Route>
+      
+    </div>
     </BrowserRouter>
       
 
