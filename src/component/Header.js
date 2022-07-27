c1e6f5a313d8ec7b3e25bd21e958b59ca383da5c
Salome <sallynanjala254@gmail.com>
2022-07-27 19:38:32 +0300

    Added a classname;Search and a cart icon

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import { Container, Navbar } from 'react-bootstrap'
+import { Badge, Container, Dropdown, FormControl, Nav, Navbar } from 'react-bootstrap'
+import {FaCartArrowDown} from 'react-icons/fa'
 
 function Header() {
   return (
-    <Navbar>
+    <Navbar bg="light" variant="light" style={{height:100}}>
         <Container>
             <Navbar.Brand>
-                <a>ShoppinKE</a>
+                <a href="/">ShoppinKE</a>
             </Navbar.Brand>
+            <Navbar.Text className='search'>
+                <FormControl style={{width:700}} placeholder="Search a product"
+                className='m-auto'
+                />
+            </Navbar.Text>
+            <Nav>
+                <Dropdown alignRight>
+                    <Dropdown.Toggle variant='Success'>
+                        *<FaCartArrowDown color="black" fontsize="25px" />
+                        <Badge>{20}</Badge>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu style={{minwidth:370}}>
+                        <span style={{padding:10}}>Cart is Empty!</span>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Nav>
         </Container>
     </Navbar>
   )
