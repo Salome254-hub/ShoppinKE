05472e96c253678bfbbf912778be687a00f3c6c4
Salome <sallynanjala254@gmail.com>
2022-07-28 23:21:53 +0300

    Added a slider in my promotions.js

--- src/components/promotions/index.js
+++ src/components/promotions/index.js
@@ -1,11 +1,41 @@
+import { Slide } from "@mui/material";
+import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
+import { MessageText, PromotionsContainer } from "../../styles/promotions";
+
 const messages = [
     "20% off on your first order!",
-    "Summer sale starts now, visit any store.",
-    "Please like and subscribe :)",
+    "Bonus sale starts now!! visit any of our stores.",
+    "Please like, share and subscribe :)",
   ];
   export default function Promotions() {
+    const[messageIndex,setMessageIndex]=useState(0)
+    const[show,setShow]=useState(true);
 
-    return
+    useEffect(() => {
+        
+const intervalId = setInterval(() => {
+  setMessageIndex((i) => (i + 1) % messages.length)},4000);
+
+  return () => {
+  clearInterval(intervalId);
+};
+}, []);
+
+    return(
+
+        <PromotionsContainer>
+            <Slide direction="left" in={show}>
+            <Box dispay={"flex"} justifyContent="center" alignItems={"center"}>
+                <MessageText>
+                    {messages[messageIndex]}
+                </MessageText>
+
+            </Box>
+        </Slide>
+
+        </PromotionsContainer>
+    )
 
 
     
---
+++ src/styles/promotions/index.js
@@ -0,0 +1,23 @@
+import {Box, styled } from "@mui/system";
+import { Colors } from "../../styles/theme";
+import { Typography } from "@mui/material";
+
+export const PromotionsContainer = styled(Box)(({ theme }) => ({
+    [theme.breakpoints.up("md")]: {
+      padding: "40px 0px 40px 0px",
+    },
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "20px 0px 20px 0px",
+    overflow: "hidden",
+    background: Colors.secondary,
+  }));
+
+  export const MessageText = styled(Typography)(({ theme }) => ({
+ [theme.breakpoints.up("md")]: {
+   fontSize: "3rem",
+ },
+ color: Colors.white,
+ fontSize: "1.5rem",
+}));
